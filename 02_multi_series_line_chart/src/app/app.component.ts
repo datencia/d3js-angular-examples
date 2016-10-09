6f1bfe3b692dc9ad85184d033f6f3017ddceff0f
David Atencia <david.atencia@gmail.com>
2016-10-09 19:58:45 +0200

    Refactor example "02 Multi-Series Line Chart"

--- 02_multi_series_line_chart/src/app/app.component.ts
+++ 02_multi_series_line_chart/src/app/app.component.ts
@@ -6,142 +6,109 @@ import * as d3Shape from "d3-shape";
 import * as d3Array from "d3-array";
 import * as d3Axis from "d3-axis";
 
+import { Temperatures } from './shared/';
+
 @Component({
   selector: 'app-root',
   template: `
   <h1>{{title}}</h1>
   <h2>{{subtitle}}</h2>
-  <svg width="600" height="400"></svg>
+  <svg width="960" height="500"></svg>
 `
 })
 export class AppComponent implements OnInit {
+
   title: string = 'D3.js with Angular 2!';
   subtitle: string = 'Multi-Series Line Chart';
 
+  data: any;
 
-    private htmlElement: HTMLElement;
-    private host: any;
-    private svg: any;
-    private container: any;
+  svg: any;
+  margin = {top: 20, right: 80, bottom: 30, left: 50};
+  g: any;
+  width: number;
+  height: number;
+  x;
+  y;
+  z;
+  line;
 
-    data = [    {"date": new Date("2016-09-01")},
-                {"date": new Date("2016-09-02")},
-                {"date": new Date("2016-09-03")},
-                {"date": new Date("2016-09-04")},
-                {"date": new Date("2016-09-05")},
-                {"date": new Date("2016-09-06")},
-                {"date": new Date("2016-09-07")},
-                {"date": new Date("2016-09-08")},
-                {"date": new Date("2016-09-09")},
-                {"date": new Date("2016-09-10")}];
+  constructor() {
 
-    cities = [
-      {
-        "id": "New York",
-        "values": [
-          {"date": new Date("2016-09-01"), "temperature": 63.4},
-          {"date": new Date("2016-09-02"), "temperature": 58.0},
-          {"date": new Date("2016-09-03"), "temperature": 53.3},
-          {"date": new Date("2016-09-04"), "temperature": 55.7},
-          {"date": new Date("2016-09-05"), "temperature": 64.2},
-          {"date": new Date("2016-09-06"), "temperature": 58.8},
-          {"date": new Date("2016-09-07"), "temperature": 57.9},
-          {"date": new Date("2016-09-08"), "temperature": 61.8},
-          {"date": new Date("2016-09-09"), "temperature": 69.3},
-          {"date": new Date("2016-09-10"), "temperature": 71.2}
-        ]
-      },
-      {
-        "id": "San Francisco",
-        "values": [
-          {"date": new Date("2016-09-01"), "temperature": 62.7},
-          {"date": new Date("2016-09-02"), "temperature": 59.9},
-          {"date": new Date("2016-09-03"), "temperature": 59.1},
-          {"date": new Date("2016-09-04"), "temperature": 58.8},
-          {"date": new Date("2016-09-05"), "temperature": 58.7},
-          {"date": new Date("2016-09-06"), "temperature": 57.0},
-          {"date": new Date("2016-09-07"), "temperature": 56.7},
-          {"date": new Date("2016-09-08"), "temperature": 56.8},
-          {"date": new Date("2016-09-09"), "temperature": 56.7},
-          {"date": new Date("2016-09-10"), "temperature": 60.1}
-        ]
-      }
-    ];
+  }
 
-    svg: any;
-    margin = {top: 20, right: 80, bottom: 30, left: 50};
-    g: any;
-    width: number;
-    height: number;
-    x;
-    y;
-    z;
-    line;
+  ngOnInit() {
 
-    constructor() {
-    }
+    this.data = Temperatures.map((v) => v.values.map((v) => v.date ))[0];
+                            //.reduce((a, b) => a.concat(b), []);
 
-    ngOnInit() {
+    this.initChart();
+    this.drawAxis();
+    this.drawPath();
+  }
 
-      this.svg = d3.select("svg");
+  private initChart(): void {
+    this.svg = d3.select("svg");
 
-      this.width = this.svg.attr("width") - this.margin.left - this.margin.right;
-      this.height = this.svg.attr("height") - this.margin.top - this.margin.bottom;
+    this.width = this.svg.attr("width") - this.margin.left - this.margin.right;
+    this.height = this.svg.attr("height") - this.margin.top - this.margin.bottom;
 
-      this.g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
+    this.g = this.svg.append("g").attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
 
-      this.x = d3Scale.scaleTime().range([0, this.width]);
-      this.y = d3Scale.scaleLinear().range([this.height, 0]);
-      this.z = d3Scale.scaleOrdinal(d3Scale.schemeCategory10);
+    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.z = d3Scale.scaleOrdinal(d3Scale.schemeCategory10);
 
-      this.line = d3Shape.line()
-                         .curve(d3Shape.curveBasis)
-                         .x( (d) => this.x(d.date) )
-                         .y( (d) => this.y(d.temperature) );
+    this.line = d3Shape.line()
+                       .curve(d3Shape.curveBasis)
+                       .x( (d: any) => this.x(d.date) )
+                       .y( (d: any) => this.y(d.temperature) );
 
+    this.x.domain(d3Array.extent(this.data, (d: Date) => d ));
 
-      this.x.domain(d3Array.extent(this.data, function(d) { return d.date; }));
+    this.y.domain([
+      d3Array.min(Temperatures, function(c) { return d3Array.min(c.values, function(d) { return d.temperature; }); }),
+      d3Array.max(Temperatures, function(c) { return d3Array.max(c.values, function(d) { return d.temperature; }); })
+    ]);
 
-      this.y.domain([
-        d3Array.min(this.cities, function(c) { return d3Array.min(c.values, function(d) { return d.temperature; }); }),
-        d3Array.max(this.cities, function(c) { return d3Array.max(c.values, function(d) { return d.temperature; }); })
-      ]);
+    this.z.domain(Temperatures.map(function(c) { return c.id; }));
+  }
 
-      this.z.domain(this.cities.map(function(c) { return c.id; }));
+  private drawAxis(): void {
+    this.g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + this.height + ")")
+      .call(d3Axis.axisBottom(this.x));
 
-      this.g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + this.height + ")")
-        .call(d3Axis.axisBottom(this.x));
+    this.g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3Axis.axisLeft(this.y))
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("fill", "#000")
+      .text("Temperature, ºF");
+  }
 
-      this.g.append("g")
-        .attr("class", "axis axis--y")
-        .call(d3Axis.axisLeft(this.y))
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("fill", "#000")
-        .text("Temperature, ºF");
+  private drawPath(): void {
+    let city = this.g.selectAll(".city")
+      .data(Temperatures)
+      .enter().append("g")
+      .attr("class", "city");
 
-      let city = this.g.selectAll(".city")
-        .data(this.cities)
-        .enter().append("g")
-        .attr("class", "city");
+    city.append("path")
+      .attr("class", "line")
+      .attr("d", (d) => this.line(d.values) )
+      .style("stroke", (d) => this.z(d.id) );
 
-      city.append("path")
-        .attr("class", "line")
-        .attr("d", (d) => this.line(d.values) )
-        .style("stroke", (d) => this.z(d.id) );
-
-      city.append("text")
-        .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-        .attr("transform", (d) => "translate(" + this.x(d.value.date) + "," + this.y(d.value.temperature) + ")" )
-        .attr("x", 3)
-        .attr("dy", "0.35em")
-        .style("font", "10px sans-serif")
-        .text(function(d) { return d.id; });
-
-    }
+    city.append("text")
+      .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+      .attr("transform", (d) => "translate(" + this.x(d.value.date) + "," + this.y(d.value.temperature) + ")" )
+      .attr("x", 3)
+      .attr("dy", "0.35em")
+      .style("font", "10px sans-serif")
+      .text(function(d) { return d.id; });
+  }
 
 }
