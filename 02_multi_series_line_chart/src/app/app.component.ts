3b179ad7b6bd6a537f047148dbc099addeb85045
David Atencia <david.atencia@gmail.com>
2017-10-28 20:47:42 +0200

    style: minor changes

--- 02_multi_series_line_chart/src/app/app.component.ts
+++ 02_multi_series_line_chart/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 import * as d3 from 'd3-selection';
-import * as d3Scale from "d3-scale";
-import * as d3Shape from "d3-shape";
-import * as d3Array from "d3-array";
-import * as d3Axis from "d3-axis";
+import * as d3Scale from 'd3-scale';
+import * as d3Shape from 'd3-shape';
+import * as d3Array from 'd3-array';
+import * as d3Axis from 'd3-axis';
 
 import { Temperatures } from './shared/';
 
